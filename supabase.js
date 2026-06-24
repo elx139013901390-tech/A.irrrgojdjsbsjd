f0b7327030715e13e48b5542bbf94daa22e3c0a4
elx139013901390-tech <elx139013901390@gmail.com>
2026-06-24 17:08:08 +0330

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,9 @@
-const { createClient } = require('@supabase/supabase-js');
+const { createClient } = supabase;
 
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+const SUPABASE_URL = "https://uvtvybpmpuiedjhmxkes.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2dHZ5YnBtcHVpZWRqaG14a2VzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzMDE0NTYsImV4cCI6MjA5Nzg3NzQ1Nn0.bME9cKnKtjE2lcRScEzQjSuWI02IZpv374cH24EJtm8";
+
+const db = createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
 );
-
-module.exports = supabase;
