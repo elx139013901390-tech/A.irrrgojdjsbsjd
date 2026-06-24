1af174927ee4f20fa75300c08bc1a4f52be4d90f
elx139013901390-tech <elx139013901390@gmail.com>
2026-06-24 18:58:32 +0330

    Create app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://jmjhfqkqcjzhuuojvkyu.supabase.co";
+
+const SUPABASE_ANON_KEY =
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImptamhmcWtxY2p6aHV1b2p2a3l1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzMDgzMDcsImV4cCI6MjA5Nzg4NDMwN30.opuxBE74VTBcvbyJ-7wQ7ybqZoAzRs5VqmLAlmCQeGg";
