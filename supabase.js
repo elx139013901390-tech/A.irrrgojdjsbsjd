bb10a9ec3c6a6e99724409e97554bad331dd1c54
elx139013901390-tech <elx139013901390@gmail.com>
2026-06-24 17:09:56 +0330

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,9 +1,8 @@
-const { createClient } = supabase;
+const { createClient } = require("@supabase/supabase-js");
 
-const SUPABASE_URL = "https://uvtvybpmpuiedjhmxkes.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2dHZ5YnBtcHVpZWRqaG14a2VzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzMDE0NTYsImV4cCI6MjA5Nzg3NzQ1Nn0.bME9cKnKtjE2lcRScEzQjSuWI02IZpv374cH24EJtm8";
-
-const db = createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
+const supabase = createClient(
+  "https://uvtvybpmpuiedjhmxkes.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2dHZ5YnBtcHVpZWRqaG14a2VzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzMDE0NTYsImV4cCI6MjA5Nzg3NzQ1Nn0.bME9cKnKtjE2lcRScEzQjSuWI02IZpv374cH24EJtm8"
 );
+
+module.exports = supabase;
