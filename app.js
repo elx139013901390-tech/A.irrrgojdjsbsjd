f66496e7de5b2d54706b29d24df039749f6219be
elx139013901390-tech <elx139013901390@gmail.com>
2026-06-24 21:37:38 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const SUPABASE_URL = "https://jmjhfqkqcjzhuuojvkyu.supabase.co";
+const SUPABASE_URL =
+"https://aorhzgfyntfybfovocle.supabase.co";
 
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImptamhmcWtxY2p6aHV1b2p2a3l1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzMDgzMDcsImV4cCI6MjA5Nzg4NDMwN30.opuxBE74VTBcvbyJ-7wQ7ybqZoAzRs5VqmLAlmCQeGg";
+const SUPABASE_KEY =
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFvcmh6Z2Z5bnRmeWJmb3ZvY2xlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzMTc4NjgsImV4cCI6MjA5Nzg5Mzg2OH0.5SAW3uwc8rJFz-mzvbLzf5TFFr2cQU82yJN_t_LotOg";
 
 const supabase =
 window.supabase.createClient(
