9fc409f76b5412626778cb1ce24ace12e4945949
elx139013901390-tech <elx139013901390@gmail.com>
2026-06-24 22:57:27 +0330

    Update app.js

--- app.js
+++ app.js
@@ -4,59 +4,68 @@ const SUPABASE_URL =
 const SUPABASE_KEY =
 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vaHFnYnFnenVmZmJhYXpxeWZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzMTM4MjEsImV4cCI6MjA5Nzg4OTgyMX0.jwvtramJ8ObtFLmqWRmd9m2jx__16395znhvBFFMlrs";
 
-alert("APP LOADED");
-
 const supabase =
 window.supabase.createClient(
 SUPABASE_URL,
 SUPABASE_KEY
 );
 
-async function registerUser() {
-
-alert("ثبت نام کلیک شد");
-
-try {
+async function registerUser(){
 
 const username =
-document.getElementById("username").value;
+document.getElementById("username").value.trim();
 
 const password =
-document.getElementById("password").value;
+document.getElementById("password").value.trim();
 
-if (!username || !password) {
+if(!username || !password){
 alert("نام کاربری و رمز عبور را وارد کنید");
 return;
 }
 
-const { data, error } =
+const { error } =
 await supabase
 .from("users")
 .insert([
 {
-username,
-password
+username: username,
+password: password
 }
-])
-.select();
+]);
 
-if (error) {
-alert("ERROR: " + error.message);
+if(error){
+alert(error.message);
 return;
 }
 
 alert("✅ ثبت نام موفق");
-
-} catch(e) {
-
-alert("JS ERROR: " + e.message);
-
 }
 
+async function login(){
+
+const username =
+document.getElementById("username").value.trim();
+
+const password =
+document.getElementById("password").value.trim();
+
+const { data,error } =
+await supabase
+.from("users")
+.select("*")
+.eq("username",username)
+.eq("password",password)
+.single();
+
+if(error || !data){
+alert("نام کاربری یا رمز عبور اشتباه است");
+return;
 }
 
-async function login() {
+localStorage.setItem(
+"user",
+JSON.stringify(data)
+);
 
-alert("ورود کلیک شد");
-
+alert("✅ ورود موفق");
 }
